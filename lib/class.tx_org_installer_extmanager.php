d780c477c1d29d908a81b4fdce7579cefc4a92ad
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-29 21:13:01 +0000

    0.1.0: extension manager
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_installer/trunk@45932 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_org_installer_extmanager.php
+++ lib/class.tx_org_installer_extmanager.php
@@ -153,7 +153,8 @@ class tx_org_installer_extmanager
     $groupBy        ='';
     $orderBy        ='';
     $limit          ='';
-    var_dump($GLOBALS['TYPO3_DB']->SELECTquery($select_fields,$from_table,$where_clause,$groupBy,$orderBy,$limit)); 
+    var_dump(__METHOD__ . ' (' . __LINE__ . '): ' . $GLOBALS['TYPO3_DB']->SELECTquery($select_fields,$from_table,$where_clause,$groupBy,$orderBy,$limit));
+    exit; 
     $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery($select_fields,$from_table,$where_clause,$groupBy,$orderBy,$limit); 
     while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res))
     {

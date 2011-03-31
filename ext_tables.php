03d7260c7b2f439392a7958fcf6f2ef6892d6294
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-31 20:41:16 +0000

    0.1.0: code formating
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_installer/trunk@46050 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -3,71 +3,47 @@ if (!defined ('TYPO3_MODE'))  die ('Access denied.');
 
 
 
-  ///////////////////////////////////////////////////////////
-  //
-  // Language for labels of static templates and page tsConfig
-
-$llStatic = $confArr['LLstatic'];
-switch($llStatic) {
-  case($llStatic == 'German'):
-    $llStatic = 'de';
-    break;
-  default:
-    $llStatic = 'default';
-}
-  // Language for labels of static templates and page tsConfig
-
-
-
-  ///////////////////////////////////////////////////////////
-  //
-  // Plugin general configuration
+    ///////////////////////////////////////////////////////////
+    //
+    // Plugin general configuration
   
   t3lib_div::loadTCA('tt_content');
-  // Plugin general configuration
+    // Plugin general configuration
 
 
 
-  ///////////////////////////////////////////////////////////
-  //
-  // Plugin 1 configuration
+    ///////////////////////////////////////////////////////////
+    //
+    // Plugin 1 configuration
   
   $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']  = 
     'layout,select_key,pages,recursive';
-  // Remove the default tt_content fields layout, select_key, pages and recursive.
+    // Remove the default tt_content fields layout, select_key, pages and recursive.
   $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']      =
     'pi_flexform';
-  // Display the field pi_flexform
+    // Display the field pi_flexform
   t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/pi1/flexform.xml');
-  // Register our file with the flexform structure
+    // Register our file with the flexform structure
   t3lib_extMgm::addPlugin(
     array(
       'LLL:EXT:org_installer/pi1/locallang.xml:tt_content.list_type_pi1', 
       $_EXTKEY  .'_pi1',
-      'EXT:org_installer/ext_icons/ext_icon_blink.gif'),
-      'list_type');
-  // Add the Flexform to the Plugin List
-  // Plugin 1 configuration
+      'EXT:org_installer/ext_icons/ext_icon_blink.gif'
+    ),
+    'list_type'
+  );
+    // Add the Flexform to the Plugin List
+    // Plugin 1 configuration
+
+
 
   ////////////////////////////////////////////////////////////////////////////
   // 
   // Add pagetree icons
 
-  // Case $llStatic
-switch(true) {
-  case($llStatic == 'de'):
-      // German
-    $TCA['pages']['columns']['module']['config']['items'][] = 
-       array('Org: Installer', 'org_inst', t3lib_extMgm::extRelPath($_EXTKEY).'ext_icons/ext_icon_blink.gif');
-    break;
-  default:
-      // English
-    $TCA['pages']['columns']['module']['config']['items'][] = 
-       array('Org: Installer', 'org_inst', t3lib_extMgm::extRelPath($_EXTKEY).'ext_icons/ext_icon_blink.gif');
-}
-  // Case $llStatic
-
-$ICON_TYPES['org_inst'] = array('icon' => t3lib_extMgm::extRelPath($_EXTKEY).'ext_icons/ext_icon_blink.gif');
-
+$TCA['pages']['columns']['module']['config']['items'][] = 
+  array('Org: Installer', 'org_inst', t3lib_extMgm::extRelPath($_EXTKEY).'ext_icons/ext_icon_blink.gif');
+$ICON_TYPES['org_inst'] = 
+  array('icon' => t3lib_extMgm::extRelPath($_EXTKEY).'ext_icons/ext_icon_blink.gif');
   // Add pagetree icons
 ?>

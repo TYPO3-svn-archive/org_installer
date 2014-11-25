daae58b8542134340e4df0dcb1a05ae9752e88c4
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-11-25 00:31:50 +0000

    6.0.2 +++ Update Manual
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org_installer/trunk@86991 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = array(
   ,
   'category' => 'plugin',
   'shy' => 0,
-  'version' => '6.0.1',
+  'version' => '6.0.2',
   'dependencies' => 'css_styled_content,org',
   'conflicts' => '',
   'priority' => '',
